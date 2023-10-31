18f67df160413d046ca9abe7525008730dac30da
neon <fatehzaliyev@gmail.com>
2023-10-31 08:53:20 +0000

    User and db service init v2

---
+++ db-service/src/main/java/com/ktxdevelopment/dbservice/DbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ktxdevelopment.dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+
+}
